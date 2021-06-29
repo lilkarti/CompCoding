261e5df632c1b56336bdf2eb6117ad199be9c062
lilkarti <kartikeys2001@gmail.com>
2021-06-29 16:50:00 -0400

    Leetcode Easy - Remove Element

--- palindrome.cpp
+++ palindrome.cpp
@@ -18,6 +18,6 @@ public class Solution {
         // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
         // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
         // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
-        return x == revertedNumber || x == revertedNumber/10;
+        return x == revertedNu mber || x == revertedNumber/10;
     }
 }
